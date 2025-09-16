79495f0382dc04caaf8025198fecd2542bc23b7b
karthikramx <karthikram570@gmail.com>
2025-09-16 23:07:22 +0530

    adding some basic comments

---
+++ javascript/Variables and Data Types/000Comments.js
@@ -0,0 +1,17 @@
+// Comments - use them to explain code to yourself or others
+
+// there are two types of comments
+
+// type 1 - single line comments
+
+/*
+type 2 - mutli line comments
+also - avoid writing long paras
+instead explain in points !
+*/
+
+// Use comments as reminder
+// TODO: this is a todo comment 
+//! BUG: Needs to be fixed!
+//? comments change colors? 
+
---
+++ javascript/Variables and Data Types/001Vars&DataTypes.js
@@ -0,0 +1,3 @@
+// This how you comment in Javascript
+console.log("here you go!")
+
