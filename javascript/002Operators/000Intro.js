c51708935ee4c4ea2bc190e1df3b14223ba3fbe3
karthikramx <karthikram570@gmail.com>
2025-09-18 12:44:59 +0530

    some basic changes

---
+++ javascript/002Operators/000Intro.js
@@ -0,0 +1,15 @@
+// Arithematic Operators 
+// + , * , - , / , **, %, ++, --
+
+// Comparision Operators -> leads to a boolean value
+// >, >=, < ,<=, !==, ===
+
+// Logical Operators -> leads to a boolean value
+// &&, || , !
+
+// Assignment Operators
+// ==
+
+// Conditional (Ternary) Operator - if a is true then apply b or c 
+// a ? b : c 
+
---
+++ javascript/002Operators/001ArithematicOperators.js
@@ -0,0 +1,35 @@
+// Arithematics Operators Demo in Javascript 
+
+// Sum of two numbers
+const sum = 10 + 1;
+console.log(sum)
+
+// Difference of two numbers
+const diff = 10 - 9;
+console.log(diff)
+
+// Division of two numbers
+const div = 10 / 3;
+console.log(div)
+
+// Multiplication of two numbers
+const mult = 10 * 3.33;
+console.log(mult)
+
+// Power 
+const pow = 10 ** 2;
+console.log(pow)
+
+// Modulus
+const mod = 10 % 3;
+console.log(mod)
+
+// Increment Operator 
+let inc = 10
+inc++;
+console.log(inc)
+
+// Decrement Operator 
+let dec = 11;
+dec--;
+console.log(dec)
