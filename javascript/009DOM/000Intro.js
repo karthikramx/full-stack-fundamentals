b53e0455c50e9c5aebe181e0125447e7b359ae22
karthikramx <karthikram570@gmail.com>
2025-09-29 09:52:41 +0530

    Added DOM concepts

---
+++ javascript/009DOM/000Intro.js
@@ -0,0 +1,13 @@
+// DOM 
+// Document Object Model 
+
+// DOM represents how a document is structured, allowing for easy access and modification 
+// DOM provides a standard way. to interacting with Objects
+// HTML DOM - focuses on how the HTML page is structured and can be modified 
+
+// HTML DOM
+// Structure: How an HTML page is organized 
+// Modification: How elements can be added , removed, or changed
+// Properties and Styles: Attributes and styles that can be appled to elements 
+
+
