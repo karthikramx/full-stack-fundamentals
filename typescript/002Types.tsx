afd52b45b8160e176e19b099fc285cd561072ffc
karthikramx <karthikram570@gmail.com>
2025-10-27 21:03:12 +0530

    adding some typescript realted code

---
+++ typescript/002Types.tsx
@@ -0,0 +1,8 @@
+const myName: string = "karthik";
+const myAge: number = 31;
+console.log(myName);
+console.log(myAge);
+
+// myAge = "31"; // this will fail
+
+// Exploring all the types in typescript!
