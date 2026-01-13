2650082d54672ee405f6e788645fa731de343107
karthikramx <karthikram570@gmail.com>
2026-01-13 08:18:50 +0530

    refactoring stuff

--- 001 Programming Fundamentals/000 Python/Finance/001Investing.py
+++ 001 Programming Fundamentals/000 Python/Finance/001Investing.py
@@ -1,14 +1,18 @@
 
 
-def invested_lumpsum_gains(monthly_contribution, yearly_stepup, years,xirr):
+def invested_lumpsum_gains(monthly_contribution, annual_stepup, years,xirr):
     lumpsum = 0
     gains = 0
     for i in range(0,years):
-        lumpsum += 12 * monthly_contribution * (yearly_stepup**i)
+        lumpsum += 12 * monthly_contribution * (annual_stepup**i)
         gains += lumpsum*xirr
-    return lumpsum,gains
+    return round(lumpsum,0),round(gains,0)
 
 
-total,gains = invested_lumpsum_gains(60000,1.1,10,1.13)
-print(total)
-print(gains)
+xirr = 1.13
+annual_stepup=1.1
+time_period = 10
+
+total,gains = invested_lumpsum_gains(60000,annual_stepup=annual_stepup,years=time_period,xirr=xirr)
+print(f"total amount invested with {annual_stepup} annual step-up:",total)
+print(f"total amount with {xirr} xirr", gains)
